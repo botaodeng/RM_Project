d3c04008106a0592d1faf1b62258e4a0019ebf0b
LitzJ <2609008122@qq.com>
2024-08-20 20:17:57 +0800

    配置RTOS

--- Core/Inc/FreeRTOSConfig.h
+++ Core/Inc/FreeRTOSConfig.h
@@ -68,14 +68,13 @@
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 7 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE                    ((size_t)15360)
+#define configTOTAL_HEAP_SIZE                    ((size_t)25600)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configGENERATE_RUN_TIME_STATS            1
 #define configUSE_TRACE_FACILITY                 1
 #define configUSE_16_BIT_TICKS                   0
 #define configUSE_MUTEXES                        1
 #define configQUEUE_REGISTRY_SIZE                8
-#define configUSE_RECURSIVE_MUTEXES              1
 #define configUSE_COUNTING_SEMAPHORES            1
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION  1
 /* USER CODE BEGIN MESSAGE_BUFFER_LENGTH_TYPE */
@@ -104,11 +103,7 @@ to exclude the API function. */
 #define INCLUDE_vTaskDelayUntil              1
 #define INCLUDE_vTaskDelay                   1
 #define INCLUDE_xTaskGetSchedulerState       1
-#define INCLUDE_xTimerPendFunctionCall       1
 #define INCLUDE_xQueueGetMutexHolder         1
-#define INCLUDE_uxTaskGetStackHighWaterMark  1
-#define INCLUDE_xTaskGetCurrentTaskHandle    1
-#define INCLUDE_eTaskGetState                1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -48,18 +48,6 @@
 
 /* USER CODE END Variables */
 osThreadId defaultTaskHandle;
-osThreadId KeyTaskHandle;
-uint32_t KeyTaskBuffer[ 128 ];
-osStaticThreadDef_t KeyTaskControlBlock;
-osThreadId LcdTaskHandle;
-uint32_t LcdTaskBuffer[ 256 ];
-osStaticThreadDef_t LcdTaskControlBlock;
-osThreadId ImuTaskHandle;
-uint32_t ImuTaskBuffer[ 1024 ];
-osStaticThreadDef_t ImuTaskControlBlock;
-osThreadId FunTestHandle;
-uint32_t FunTestBuffer[ 128 ];
-osStaticThreadDef_t FunTestControlBlock;
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
@@ -67,10 +55,6 @@ osStaticThreadDef_t FunTestControlBlock;
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void const * argument);
-void KeyTask_Entry(void const * argument);
-void LcdTask_Entry(void const * argument);
-void ImuTask_Entry(void const * argument);
-void FunTest_Entry(void const * argument);
 
 extern void MX_USB_DEVICE_Init(void);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
@@ -155,22 +139,6 @@ void MX_FREERTOS_Init(void) {
   osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
-  /* definition and creation of KeyTask */
-  osThreadStaticDef(KeyTask, KeyTask_Entry, osPriorityRealtime, 0, 128, KeyTaskBuffer, &KeyTaskControlBlock);
-  KeyTaskHandle = osThreadCreate(osThread(KeyTask), NULL);
-
-  /* definition and creation of LcdTask */
-  osThreadStaticDef(LcdTask, LcdTask_Entry, osPriorityNormal, 0, 256, LcdTaskBuffer, &LcdTaskControlBlock);
-  LcdTaskHandle = osThreadCreate(osThread(LcdTask), NULL);
-
-  /* definition and creation of ImuTask */
-  osThreadStaticDef(ImuTask, ImuTask_Entry, osPriorityHigh, 0, 1024, ImuTaskBuffer, &ImuTaskControlBlock);
-  ImuTaskHandle = osThreadCreate(osThread(ImuTask), NULL);
-
-  /* definition and creation of FunTest */
-  osThreadStaticDef(FunTest, FunTest_Entry, osPriorityBelowNormal, 0, 128, FunTestBuffer, &FunTestControlBlock);
-  FunTestHandle = osThreadCreate(osThread(FunTest), NULL);
-
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
@@ -195,78 +163,6 @@ void StartDefaultTask(void const * argument)
     osDelay(1);
   }
   /* USER CODE END StartDefaultTask */
-}
-
-/* USER CODE BEGIN Header_KeyTask_Entry */
-/**
-* @brief Function implementing the KeyTask thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_KeyTask_Entry */
-__weak void KeyTask_Entry(void const * argument)
-{
-  /* USER CODE BEGIN KeyTask_Entry */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END KeyTask_Entry */
-}
-
-/* USER CODE BEGIN Header_LcdTask_Entry */
-/**
-* @brief Function implementing the LcdTask thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_LcdTask_Entry */
-__weak void LcdTask_Entry(void const * argument)
-{
-  /* USER CODE BEGIN LcdTask_Entry */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END LcdTask_Entry */
-}
-
-/* USER CODE BEGIN Header_ImuTask_Entry */
-/**
-* @brief Function implementing the ImuTask thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_ImuTask_Entry */
-__weak void ImuTask_Entry(void const * argument)
-{
-  /* USER CODE BEGIN ImuTask_Entry */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END ImuTask_Entry */
-}
-
-/* USER CODE BEGIN Header_FunTest_Entry */
-/**
-* @brief Function implementing the FunTest thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_FunTest_Entry */
-__weak void FunTest_Entry(void const * argument)
-{
-  /* USER CODE BEGIN FunTest_Entry */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END FunTest_Entry */
 }
 
 /* Private application code --------------------------------------------------*/
