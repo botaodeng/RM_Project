48cd0e7e6d3381951ffb0df57de0791d25720ae1
LitzJ <2609008122@qq.com>
2024-11-29 09:40:04 +0800

    发现已知问题：新版本CubeMX生成之后，FDCAN_RxHeaderTypeDef 的DataLength定义会变，bsp_can.c需要修改

--- bsp/can/bsp_can.c
+++ bsp/can/bsp_can.c
@@ -273,7 +273,7 @@ static void FDCANFIFOxCallback(FDCAN_HandleTypeDef *_hfdcan, uint32_t fifox)
     while (HAL_FDCAN_GetRxFifoFillLevel(_hfdcan, fifox)) // FIFO不为空,有可能在其他中断时有多帧数据进入
     {
         HAL_FDCAN_GetRxMessage(_hfdcan, fifox, &rxconf, fdcan_rx_buff); // 从FIFO中获取数据
-		//解析数据长度
+		//解析数据长度，@Todo 此处在用新版本重新生成后可能得修改，DataLength可能不需要右移，具体情况具体看	！
 		if(((rxconf.DataLength >> 16) & 0xF)>=0 && ((rxconf.DataLength >> 16) & 0xF)<=8)
 		{
 			DataLength=(rxconf.DataLength >> 16) & 0xF; // 保存接收到的数据长度
