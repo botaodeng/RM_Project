616d492273d780c2d2400b2b71a1388f3708665b
botaodeng <botaoalexdeng@gmail.com>
2026-06-19 22:16:50 -0700

    把遥控器改成FS i6x解析模式

--- modules/remote/remote_control.c
+++ modules/remote/remote_control.c
@@ -6,7 +6,7 @@
 #include "daemon.h"
 #include "bsp_log.h"
 
-#define REMOTE_CONTROL_FRAME_SIZE 18u // 遥控器接收的buffer大小
+#define REMOTE_CONTROL_FRAME_SIZE 25u // 遥控器接收的buffer大小
 
 // 遥控器数据
 static RC_ctrl_t rc_ctrl[2];     //[0]:当前数据TEMP,[1]:上一次的数据LAST.用于按键持续按下和切换的判断
@@ -17,14 +17,14 @@ static USARTInstance *rc_usart_instance;
 static DaemonInstance *rc_daemon_instance;
 
 /**
- * @brief 矫正遥控器摇杆的值,超过660或者小于-660的值都认为是无效值,置0
+ * @brief 矫正遥控器摇杆的值,超过800或者小于-800的值都认为是无效值,置0
  *
  */
 static void RectifyRCjoystick()
 {
     for (uint8_t i = 0; i < 5; ++i)
-        if (abs(*(&rc_ctrl[TEMP].rc.rocker_l_ + i)) > 660)
-            *(&rc_ctrl[TEMP].rc.rocker_l_ + i) = 0;
+        if (abs(*(&rc_ctrl[TEMP].rocker_l_ + i)) > 800)
+            *(&rc_ctrl[TEMP].rocker_l_ + i) = 0;
 }
 
 /**
@@ -35,55 +35,27 @@ static void RectifyRCjoystick()
 static void sbus_to_rc(const uint8_t *sbus_buf)
 {
     // 摇杆,直接解算时减去偏置
-    rc_ctrl[TEMP].rc.rocker_r_ = ((sbus_buf[0] | (sbus_buf[1] << 8)) & 0x07ff) - RC_CH_VALUE_OFFSET;                              //!< Channel 0
-    rc_ctrl[TEMP].rc.rocker_r1 = (((sbus_buf[1] >> 3) | (sbus_buf[2] << 5)) & 0x07ff) - RC_CH_VALUE_OFFSET;                       //!< Channel 1
-    rc_ctrl[TEMP].rc.rocker_l_ = (((sbus_buf[2] >> 6) | (sbus_buf[3] << 2) | (sbus_buf[4] << 10)) & 0x07ff) - RC_CH_VALUE_OFFSET; //!< Channel 2
-    rc_ctrl[TEMP].rc.rocker_l1 = (((sbus_buf[4] >> 1) | (sbus_buf[5] << 7)) & 0x07ff) - RC_CH_VALUE_OFFSET;                       //!< Channel 3
-    rc_ctrl[TEMP].rc.dial = ((sbus_buf[16] | (sbus_buf[17] << 8)) & 0x07FF) - RC_CH_VALUE_OFFSET;                                 // 左侧拨轮
+    rc_ctrl[TEMP].rocker_r_ = ((sbus_buf[1] | (sbus_buf[2] << 8)) & 0x07ff) - RC_CH_VALUE_OFFSET;                              //!< Channel 0
+    rc_ctrl[TEMP].rocker_l1 = (((sbus_buf[2] >> 3) | (sbus_buf[3] << 5)) & 0x07ff) - RC_CH_VALUE_OFFSET;                       //!< Channel 1
+    rc_ctrl[TEMP].rocker_r1 = (((sbus_buf[3] >> 6) | (sbus_buf[4] << 2) | (sbus_buf[5] << 10)) & 0x07ff) - RC_CH_VALUE_OFFSET; //!< Channel 2
+    rc_ctrl[TEMP].rocker_l_ = (((sbus_buf[5] >> 1) | (sbus_buf[6] << 7)) & 0x07ff) - RC_CH_VALUE_OFFSET;                       //!< Channel 3
     RectifyRCjoystick();
-    // 开关,0左1右
-    rc_ctrl[TEMP].rc.switch_right = ((sbus_buf[5] >> 4) & 0x0003);     //!< Switch right
-    rc_ctrl[TEMP].rc.switch_left = ((sbus_buf[5] >> 4) & 0x000C) >> 2; //!< Switch left
+    
+    // VRA VRB,直接解算
+    rc_ctrl[TEMP].vra = (((sbus_buf[6] >> 4) | (sbus_buf[7] << 4)) & 0x07FF) - RC_CH_VALUE_OFFSET;                             //!< Channel 4
+    rc_ctrl[TEMP].vrb = (((sbus_buf[7] >> 7) | (sbus_buf[8] << 1) | (sbus_buf[9] << 9)) & 0x07ff) - RC_CH_VALUE_OFFSET;        //!< Channel 5
 
-    // 鼠标解析
-    rc_ctrl[TEMP].mouse.x = (sbus_buf[6] | (sbus_buf[7] << 8)); //!< Mouse X axis
-    rc_ctrl[TEMP].mouse.y = (sbus_buf[8] | (sbus_buf[9] << 8)); //!< Mouse Y axis
-    rc_ctrl[TEMP].mouse.press_l = sbus_buf[12];                 //!< Mouse Left Is Press ?
-    rc_ctrl[TEMP].mouse.press_r = sbus_buf[13];                 //!< Mouse Right Is Press ?
+    // 开关
+    rc_ctrl[TEMP].switch_swa = (((sbus_buf[9] >> 2) | (sbus_buf[10] << 6)) & 0x07FF);      //!< Switch left most
+    rc_ctrl[TEMP].switch_swb = (((sbus_buf[10] >> 5) | (sbus_buf[11] << 3)) & 0x07FF);     //!< Switch left middle
+    rc_ctrl[TEMP].switch_swc = ((sbus_buf[12] | (sbus_buf[13] << 8)) & 0x07FF);            //!< Switch right middle
+    rc_ctrl[TEMP].switch_swd = ((sbus_buf[13] >> 3 | (sbus_buf[14] << 5)) & 0x07FF);       //!< Switch right most
 
-    //  位域的按键值解算,直接memcpy即可,注意小端低字节在前,即lsb在第一位,msb在最后
-    *(uint16_t *)&rc_ctrl[TEMP].key[KEY_PRESS] = (uint16_t)(sbus_buf[14] | (sbus_buf[15] << 8));
-    if (rc_ctrl[TEMP].key[KEY_PRESS].ctrl) // ctrl键按下
-        rc_ctrl[TEMP].key[KEY_PRESS_WITH_CTRL] = rc_ctrl[TEMP].key[KEY_PRESS];
+    if ((sbus_buf[23] == 0x0C) | (sbus_buf[23] == 0x04) | (sbus_buf[23] == 0x08)) // sbus协议中第24个字节为0x0C表示数据不正常，但是接收器有可能还在发包
+        rc_ctrl[TEMP].online_flag = 0;
     else
-        memset(&rc_ctrl[TEMP].key[KEY_PRESS_WITH_CTRL], 0, sizeof(Key_t));
-    if (rc_ctrl[TEMP].key[KEY_PRESS].shift) // shift键按下
-        rc_ctrl[TEMP].key[KEY_PRESS_WITH_SHIFT] = rc_ctrl[TEMP].key[KEY_PRESS];
-    else
-        memset(&rc_ctrl[TEMP].key[KEY_PRESS_WITH_SHIFT], 0, sizeof(Key_t));
-
-    uint16_t key_now = rc_ctrl[TEMP].key[KEY_PRESS].keys,                   // 当前按键是否按下
-        key_last = rc_ctrl[LAST].key[KEY_PRESS].keys,                       // 上一次按键是否按下
-        key_with_ctrl = rc_ctrl[TEMP].key[KEY_PRESS_WITH_CTRL].keys,        // 当前ctrl组合键是否按下
-        key_with_shift = rc_ctrl[TEMP].key[KEY_PRESS_WITH_SHIFT].keys,      //  当前shift组合键是否按下
-        key_last_with_ctrl = rc_ctrl[LAST].key[KEY_PRESS_WITH_CTRL].keys,   // 上一次ctrl组合键是否按下
-        key_last_with_shift = rc_ctrl[LAST].key[KEY_PRESS_WITH_SHIFT].keys; // 上一次shift组合键是否按下
-
-    for (uint16_t i = 0, j = 0x1; i < 16; j <<= 1, i++)
-    {
-        if (i == 4 || i == 5) // 4,5位为ctrl和shift,直接跳过
-            continue;
-        // 如果当前按键按下,上一次按键没有按下,且ctrl和shift组合键没有按下,则按键按下计数加1(检测到上升沿)
-        if ((key_now & j) && !(key_last & j) && !(key_with_ctrl & j) && !(key_with_shift & j))
-            rc_ctrl[TEMP].key_count[KEY_PRESS][i]++;
-        // 当前ctrl组合键按下,上一次ctrl组合键没有按下,则ctrl组合键按下计数加1(检测到上升沿)
-        if ((key_with_ctrl & j) && !(key_last_with_ctrl & j))
-            rc_ctrl[TEMP].key_count[KEY_PRESS_WITH_CTRL][i]++;
-        // 当前shift组合键按下,上一次shift组合键没有按下,则shift组合键按下计数加1(检测到上升沿)
-        if ((key_with_shift & j) && !(key_last_with_shift & j))
-            rc_ctrl[TEMP].key_count[KEY_PRESS_WITH_SHIFT][i]++;
-    }
-
+        rc_ctrl[TEMP].online_flag = 1;
+    
     memcpy(&rc_ctrl[LAST], &rc_ctrl[TEMP], sizeof(RC_ctrl_t)); // 保存上一次的数据,用于按键持续按下和切换的判断
 }
 
@@ -130,7 +102,7 @@ RC_ctrl_t *RemoteControlInit(UART_HandleTypeDef *rc_usart_handle)
 
 uint8_t RemoteControlIsOnline()
 {
-    if (rc_init_flag)
+    if (rc_init_flag | !rc_ctrl[TEMP].online_flag) // 遥控器尚未初始化或者接收数据出错都视为离线
         return DaemonIsOnline(rc_daemon_instance);
     return 0;
 }
